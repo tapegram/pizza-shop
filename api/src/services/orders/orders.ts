4e70b07fb7679039ffdcb56ac89932dd4fed165c
tavish <tapegram@gmail.com>
2023-09-25 18:12:18 -0500

    Writing some validators

--- api/src/services/orders/orders.ts
+++ api/src/services/orders/orders.ts
@@ -1,8 +1,12 @@
+import * as EmailValidator from 'email-validator'
+import { phone } from 'phone'
 import type {
   QueryResolvers,
   MutationResolvers,
   OrderRelationResolvers,
 } from 'types/graphql'
+
+import { validate, validateWith } from '@redwoodjs/api'
 
 import { db } from 'src/lib/db'
 
@@ -16,10 +20,116 @@ export const order: QueryResolvers['order'] = ({ id }) => {
   })
 }
 
-export const createOrder: MutationResolvers['createOrder'] = ({ input }) => {
+export const createOrder: MutationResolvers['createOrder'] = async ({
+  input,
+}) => {
+  /**
+   * This validation block does a lot of separate round trips to the DB which could become a scaling problem in the future.
+   *
+   * if that ever happens, we can refactor to do a single round trip at the beginning before validating those records. But that's
+   * kind of ugly so punting on that!
+   */
+  const styleId = await validateStyle(input.style)
+  const sizeId = await validateSize(input.size)
+  const toppings = await validateToppings(input.toppings)
+  const customerName = validateAndFormatCustomerName(input.customerName)
+  const phoneNumber = validateAndFormatPhoneNumber(input.customerPhoneNumber)
+  const email = validateAndFormatEmail(input.customerEmail)
+  // email: [notNull, basic email validation]
+  // delivery: [notNull]
+  // All of the following address info should be required if delivery is true, otherwise we can ignore
+  // streetAddress1: [notEmpty, notNull]
+  // streetAddress2: []
+  // city: [notEmpty, notNull]
+  // state: [notEmpty, notNull], should format? Maybe should be a dropdown
+  // all input should be trimmed
   return db.order.create({
     data: input,
   })
+}
+
+const validateAndFormatEmail = (email: string) => {
+  const result: boolean = EmailValidator.validate(email)
+
+  if (!result) {
+    throw 'Invalid email address'
+  }
+
+  return email.trim().toUpperCase()
+}
+
+const validateAndFormatPhoneNumber = (phoneNumber: string) => {
+  const result = phone(phoneNumber)
+
+  if (!result.isValid) {
+    throw 'Invalid phone number'
+  }
+
+  return result.phoneNumber
+}
+
+const validateAndFormatCustomerName = (customerName: string) => {
+  validate(customerName, 'Name', {
+    presence: true,
+    length: { min: 2 }, // Requring at least a couple characters of the name. Hopefully there aren't too many single letter names out there
+  })
+
+  return customerName.trim().toUpperCase()
+}
+
+const validateToppings = async (toppingIds: number[]) => {
+  await validateWith(async () => {
+    const records = await db.pizzaTopping.findMany({
+      where: { id: { in: toppingIds } },
+    })
+    // It's a little hard to give helpful error messages purely on the server side
+    // for missing toppings, since we only have the id, which doesnt exist,
+    // and no name. So just giving a generic error :(
+    if (records.length !== toppingIds.length) {
+      const missingIds = toppingIds.filter(
+        (id) => !records.map((record) => record.id).includes(id)
+      )
+      throw 'These pizza toppings were not found ' + missingIds.join(', ')
+    }
+    const unavailableToppings = records.filter((record) => !record.isAvailable)
+    if (unavailableToppings.length > 0) {
+      throw `Some requested toppings are no longer available. Sorry about that! (${unavailableToppings})`
+    }
+  })
+
+  return toppingIds
+}
+
+const validateStyle = async (styleId: number) => {
+  await validateWith(async () => {
+    const record = await db.pizzaType.findUnique({
+      where: { id: styleId },
+    })
+    if (!record) {
+      throw 'The requested pizza style is ILLEGAL'
+    }
+    if (!record.isAvailable) {
+      throw 'The requested pizza style is no longer available. Sorry about that!'
+    }
+  })
+
+  return styleId
+}
+
+const validateSize = async (sizeId: number) => {
+  await validateWith(async () => {
+    const record = await db.pizzaSize.findUnique({
+      where: { id: sizeId },
+    })
+    if (!record) {
+      throw 'The requested pizza size is ILLEGAL'
+    }
+    if (!record.isAvailable) {
+      throw 'The requested pizza size is no longer available. Sorry about that!'
+    }
+  })
+
+  return sizeId
 }
 
 export const updateOrder: MutationResolvers['updateOrder'] = ({
