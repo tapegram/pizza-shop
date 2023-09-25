48e9a221716932b42d92f390cd2148531b86f841
tavish <tapegram@gmail.com>
2023-09-25 10:44:21 -0500

    regenerate scaffold to use singular naming like redwood wants

--- api/db/migrations/20230925154414_scaffold_db_schema/migration.sql
+++ api/db/migrations/20230925154414_scaffold_db_schema/migration.sql
@@ -5,21 +5,21 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
-CREATE TABLE "PizzaToppings" (
+CREATE TABLE "PizzaTopping" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "is_available" BOOLEAN NOT NULL
 );
 
 -- CreateTable
-CREATE TABLE "PizzaSizes" (
+CREATE TABLE "PizzaSize" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "is_available" BOOLEAN NOT NULL
 );
 
 -- CreateTable
-CREATE TABLE "PizzaTypes" (
+CREATE TABLE "PizzaType" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "is_available" BOOLEAN NOT NULL
@@ -35,8 +35,8 @@ CREATE TABLE "Order" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     CONSTRAINT "Order_deliveryId_fkey" FOREIGN KEY ("deliveryId") REFERENCES "Delivery" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
-    CONSTRAINT "Order_pizzaTypeId_fkey" FOREIGN KEY ("pizzaTypeId") REFERENCES "PizzaTypes" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Order_pizzaSizeId_fkey" FOREIGN KEY ("pizzaSizeId") REFERENCES "PizzaSizes" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "Order_pizzaTypeId_fkey" FOREIGN KEY ("pizzaTypeId") REFERENCES "PizzaType" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Order_pizzaSizeId_fkey" FOREIGN KEY ("pizzaSizeId") REFERENCES "PizzaSize" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -69,30 +69,30 @@ CREATE TABLE "Address" (
 );
 
 -- CreateTable
-CREATE TABLE "_OrderToPizzaToppings" (
+CREATE TABLE "_OrderToPizzaTopping" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL,
-    CONSTRAINT "_OrderToPizzaToppings_A_fkey" FOREIGN KEY ("A") REFERENCES "Order" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "_OrderToPizzaToppings_B_fkey" FOREIGN KEY ("B") REFERENCES "PizzaToppings" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "_OrderToPizzaTopping_A_fkey" FOREIGN KEY ("A") REFERENCES "Order" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_OrderToPizzaTopping_B_fkey" FOREIGN KEY ("B") REFERENCES "PizzaTopping" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "PizzaToppings_name_key" ON "PizzaToppings"("name");
+CREATE UNIQUE INDEX "PizzaTopping_name_key" ON "PizzaTopping"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "PizzaSizes_name_key" ON "PizzaSizes"("name");
+CREATE UNIQUE INDEX "PizzaSize_name_key" ON "PizzaSize"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "PizzaTypes_name_key" ON "PizzaTypes"("name");
+CREATE UNIQUE INDEX "PizzaType_name_key" ON "PizzaType"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "CustomerInfo_orderId_key" ON "CustomerInfo"("orderId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "_OrderToPizzaToppings_AB_unique" ON "_OrderToPizzaToppings"("A", "B");
+CREATE UNIQUE INDEX "_OrderToPizzaTopping_AB_unique" ON "_OrderToPizzaTopping"("A", "B");
 
 -- CreateIndex
-CREATE INDEX "_OrderToPizzaToppings_B_index" ON "_OrderToPizzaToppings"("B");
+CREATE INDEX "_OrderToPizzaTopping_B_index" ON "_OrderToPizzaTopping"("B");
